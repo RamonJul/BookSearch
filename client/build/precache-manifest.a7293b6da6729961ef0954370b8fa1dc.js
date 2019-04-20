154a4d21ea868bb8634bff66be404ad3a0763c06
RamonJul <ramoncarlosjulian@gmail.com>
2019-04-20 14:37:20 -0400

    deploy

---
+++ client/build/precache-manifest.a7293b6da6729961ef0954370b8fa1dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7dbfc044b355db836857",
+    "url": "/static/js/main.dafc703d.chunk.js"
+  },
+  {
+    "revision": "fa8083edd493237ddb71",
+    "url": "/static/js/2.dd77253f.chunk.js"
+  },
+  {
+    "revision": "7dbfc044b355db836857",
+    "url": "/static/css/main.749250a1.chunk.css"
+  },
+  {
+    "revision": "184cdd163598b1cf145023ca2324803c",
+    "url": "/index.html"
+  }
+];
